17d3fb2f11b47fa1933bf49aa5384b3cb1b3b42d
Patrick Flanagan <patrickflanagan@patricks-imac.local>
2017-12-09 23:19:49 -0600

    Logic addition so that images load to Detail View Controllers.

--- HackingWithSwift - Project1/DetailViewController.swift
+++ HackingWithSwift - Project1/DetailViewController.swift
@@ -13,9 +13,15 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
+    var selectedImage: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+        
         // Do any additional setup after loading the view.
     }
 
